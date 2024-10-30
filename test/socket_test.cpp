4deead254867515c80e0f9fbaa223ef44dc1c564
vocal <48099292+coolest@users.noreply.github.com>
2024-10-30 18:45:23 -0500

    Fixed testing

--- test/socket_test.cpp
+++ test/socket_test.cpp
@@ -6,20 +6,25 @@ int main(){
 
     std::cout << "here\n";
 
-    network::https_socket socket{"google.com"};
+    network::https_socket socket{"www.google.com", 443};
     network::connect_response r = socket.connect();
     std::cout << r << std::endl;
     std::cout << socket.get_dns_host().err << std::endl;
 
     socket.send_string(
-        "GET /  HTTP/1.1\r\nHost:" + socket.get_dns_host().host + "\r\n\r\n",
+        "GET / HTTP/1.1\r\n"
+        "Host: www.google.com\r\n"
+        "Connection: close\r\n"
+        "User-Agent: Mozilla/5.0\r\n"
+        "\r\n",
         ignore_flag
     );
 
     std::cout << "here\n" << ignore_flag << std::endl;
 
     std::string res{};
-    socket.receive_string(res, ignore_flag);
+    network::receive_response r2 = socket.receive_string(res, ignore_flag);
+    std::cout << r2 << " " << ignore_flag << std::endl;
 
     std::cout << res << std::endl;
 }
